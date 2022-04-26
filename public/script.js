76d2862b78cf73b866a184ba1efcae8f57a90dc9
VertexPlays <rjgillette@yahoo.com>
2022-04-26 14:23:05 -0400

    made hangman

--- public/script.js
+++ public/script.js
@@ -18,3 +18,35 @@ socket.on('chat message', function(msg) {
     messages.appendChild(item);
     window.scrollTo(0, document.body.scrollHeight);
 });
+
+
+const canvas = document.getElementById("canvas");
+const c = canvas.getContext("2d")
+
+
+let pos = {};
+let mousedown = false
+document.addEventListener("mousemove", e => {
+    pos.x = e.clientX;
+    pos.y = e.clientY
+    if(mousedown){
+        draw()
+
+    }
+})
+
+document.addEventListener("mousedown", (e) => mousedown = true);
+document.addEventListener("mouseup", (e) => mousedown = false)
+function draw (){
+    c.beginPath();
+
+    c.lineWidth = 5;
+    c.lineCap = 'round'
+    c.sttrokeStyle = '#c0392h'
+
+    c.moveTo(pos.x, pos.y);
+    
+    c.lineTo(pos.x, pos.y)
+
+    c.stroke();
+}
