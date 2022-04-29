89a71dd20d2a2dd95d2d6d49a6528d1b5b838b9d
VertexPlays <rjgillette@yahoo.com>
2022-04-29 14:24:46 -0400

    cool schtuffs

--- public/socket.js
+++ public/socket.js
@@ -1,5 +1,10 @@
 const socket = io("http://localhost:3000");
 
 socket.on("connect", () => {
-    console.log("CONNECTED");
+    const username = prompt("Please Enter A Username", "user5301")
+    socket.emit("new-user", username)
+})
+
+socket.on("new-player", players => {
+    
 })
